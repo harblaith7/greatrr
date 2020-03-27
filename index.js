16bc001e9c582cc8320cacc363a58288135f3b8a
Laith Harb <harblaith77@gmail.com>
2020-03-27 16:24:20 -0400

    Configured client so that it can deploy onto Heroku, hopefully

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express');
 
 const app = express()
-const PORT = process.env.PORT || 8080
-
-app.get('/', (req, res) => {
-    res.send("Can this shit please work, it works but hopefully it works so far but hopefully it works with the react app")
-})
+const PORT = process.env.PORT || 8080 
 
 app.get('/about', (req, res) => {
     res.send("This is the about page")
 })
+
+// RAN IF IN PRODUCTION     
+if (process.env.NODE_ENV === "production"){
+    // Express will seeve up produciton assets
+    app.use(express.static('client/build'))
+
+    const path = require('path')
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Now running on port ${PORT}`)
