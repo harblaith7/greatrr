49eb010c7c0fb65de47730a613f3e1d22aaa10bf
Laith Harb <harblaith77@gmail.com>
2020-03-28 06:08:29 -0400

    Completed about design

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import "./About.scss"
+import "./About.scss";
+import clock from "../../assets/svg/hour.svg";
+import calendar from "../../assets/svg/calendar.svg"
+import target from "../../assets/svg/target.svg"
 
 class About extends Component {
     render() {
@@ -8,13 +11,42 @@ class About extends Component {
                 <div className="About__container">
                     <div className="About__description-container">
                         <h2 className="About__heading">
-                            How we work
+                            What we do
                         </h2>
                         <p className="About__description">
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                         Amet at magnam asperiores, consequatur quasi atque tempora optio architecto alias
                         aperiam nihil vitae veniam veritatis laboriosam similique, placeat iste numquam saepe nostrum nisi accusantium, minus natus! Et ab dolorum omnis dolores.
                         </p>
+                    </div>
+                    <div className="About__card-container">
+                        <div className="About__card">
+                            <img src={clock} alt="" className="About__card-icon"/>
+                            <h4 className="About__card-heading">
+                                Measure efficiency to find out how you are using your time
+                            </h4>
+                            <div className="About__card-description">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda facere error excepturi similique fuga impedit aut quisquam voluptatibus est repellendus!
+                            </div>
+                        </div>
+                        <div className="About__card">
+                            <img src={calendar} alt="" className="About__card-icon"/>
+                            <h4 className="About__card-heading">
+                                Monitor how often you complete your daily habits
+                            </h4>
+                            <div className="About__card-description">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda facere error excepturi similique fuga impedit aut quisquam voluptatibus est repellendus!
+                            </div>
+                        </div>
+                        <div className="About__card">
+                            <img src={target} alt="" className="About__card-icon"/>
+                            <h4 className="About__card-heading">
+                                Stay accountable and motivated towards your goals
+                            </h4>
+                            <div className="About__card-description">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda facere error excepturi similique fuga impedit aut quisquam voluptatibus est repellendus!
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
