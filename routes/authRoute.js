9b71d84537a93e4b17e993d3899353a2d28dba42
Laith Harb <harblaith77@gmail.com>
2020-03-29 17:39:52 -0400

    Completed OAuth backend flow for production

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -4,6 +4,10 @@ module.exports = (app) => {
     app.get("/auth/google", passport.authenticate('google', {
         scope: ['profile', 'email']
     }))
+
+    // mongodb+srv://greatrr-prod:<password>@cluster0-jrdez.mongodb.net/test?retryWrites=true&w=majority
+    // 403064708681-i5cmsoh6dd3pmpdugi3kr190tots2sbg.apps.googleusercontent.com
+    // EmwmdAjOu3NYMNayKzClaS0I
     
     app.get('/auth/google/callback', passport.authenticate('google'))
 
