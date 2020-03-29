a713f7de309e94d62c3c47cbcc3f4f0ba39f3bf6
Laith Harb <harblaith77@gmail.com>
2020-03-29 13:13:49 -0400

    Completed backend OAuth flow in development environment

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require('express');
+const passport = require('passport');
+const cookieSession = require("cookie-session");
+const keys = require('./config/keys')
+const mongoose = require('mongoose')
+require("./model/User")
+require("./services/passport")
+
+mongoose.connect(keys.mongoURI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+ })
 
 
 const app = express()
-const PORT = process.env.PORT || 8080 
 
-app.get('/about', (req, res) => {
-    res.send("This is the about page")
-})
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+// ROUTES 
+require("./routes/authRoute")(app)
 
 
 
@@ -20,6 +41,8 @@ if (process.env.NODE_ENV === "production"){
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
+
+const PORT = process.env.PORT || 8080 
 
 app.listen(PORT, () => {
     console.log(`Now running on port ${PORT}`)
--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,43 @@
 const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys')
+const mongoose = require('mongoose')
 
+const User = mongoose.model("users")
 
-passport.use(new GoogleStrategy({
-    
-}))
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+  .then(user => {
+      done(null, user)
+  })
+})
+
+passport.use(
+    new GoogleStrategy({
+        clientID : keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: "/auth/google/callback"
+    }, (accessToken, refreshToken, profile, done) => {
+
+        User.findOne({
+            googleId : profile.id
+        })
+        .then((exisitngUser) => {
+            if(exisitngUser){
+                done(null, exisitngUser)
+            } else {
+                new User({
+                    googleId : profile.id
+                })
+                .save()
+                .then(user => done(null, user))
+            }
+        })
+
+        
+    })
+)
