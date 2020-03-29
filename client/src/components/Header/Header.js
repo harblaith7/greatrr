08d89e15b65201501c7597b81d3d75c65367d883
Laith Harb <harblaith77@gmail.com>
2020-03-29 18:53:01 -0400

    Added setUpProxy redirect for development

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -17,9 +17,9 @@ class Header extends Component {
                             Sed ipsum vitae voluptatum repudiandae, sapiente dolores!
                         </p>
                         <div className="Header__btn-container">
-                            <button className="Header__sign-in-btn Header__sign-in-btn--google">
+                            <a href="/auth/google" className="Header__sign-in-btn Header__sign-in-btn--google">
                                 Sign in with Google
-                            </button>
+                            </a>
                             <button className="Header__sign-in-btn Header__sign-in-btn--facebook">
                                 Sign in with Facebook
                             </button>
