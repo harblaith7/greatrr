5311d71ff5ac4fe78fd9d2e2b79ccbff0a62f209
Laith Harb <harblaith77@gmail.com>
2020-04-01 14:45:18 -0400

    Completed styling and some front-end functionality for individual habit section

--- client/src/components/IndividualHabit/IndividualHabit.js
+++ client/src/components/IndividualHabit/IndividualHabit.js
@@ -3,20 +3,22 @@ import "./IndividualHabit.scss"
 
 class IndividualHabit extends Component {
     render() {
+        const {color, currentScore, duration} = this.props.habitInfo
         return (
-            <div className="IndividualHabit">
+            <div className="IndividualHabit" >
+                <div className="IndividualHabit__color" style={{backgroundColor: color, width : `${currentScore/duration * 117.5}%`}}/>
                 <div className="IndividualHabit__container">
                     <div className="IndividualHabit__description-container">
                         <div className="IndividualHabit__heading-container">
                             <h3 className="IndividualHabit__habit-title">
-                                Wake up Early
+                                {this.props.habitInfo.habit}
                             </h3>
                             <p className="IndividualHabit__habit-description">
-                                Out of bed by 5AM
+                                {this.props.habitInfo.description}
                             </p>
                         </div>
                         <h2 className="IndividualHabit__count">
-                            5
+                            {this.props.habitInfo.currentScore}
                         </h2>
                     </div>
                 </div>
--- client/src/components/StatsHeader/StatsHeader.js
+++ client/src/components/StatsHeader/StatsHeader.js
@@ -3,18 +3,64 @@ import "./StatsHeader.scss"
 import IndividualHabit from "../IndividualHabit/IndividualHabit"
 
 class StatsHeader extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            habits : [
+                {
+                    habit: "Wake up Early",
+                    description: "Out of bed by 5AM",
+                    duration: 5,
+                    currentScore: 3,
+                    color: "green"
+                },
+                {
+                    habit: "Workout",
+                    description: "1 hour of weight training",
+                    duration: 6,
+                    currentScore: 6,
+                    color: "pink"
+                },
+                {
+                    habit: "Read",
+                    description: "30 minutes of reading",
+                    duration: 3,
+                    currentScore: 1,
+                    color: "yellow"
+                },
+                {
+                    habit: "Meditate",
+                    description: "45 minutes of meditation in the morning",
+                    duration: 6,
+                    currentScore: 4,
+                    color: "lightblue"
+                },
+                {
+                    habit: "Basketball",
+                    description: "2 hours of training",
+                    duration: 5,
+                    currentScore: 4,
+                    color: "brown"
+                },
+
+            ]
+        }
+    }
+
+    displayHabits = () => {
+        return this.state.habits.map(habit => {
+            return <IndividualHabit habitInfo = {habit} />
+        })
+    }
+
     render() {
         return (
             <div className="StatsHeader">
                 <div className="StatsHeader__container">
                     <div className="StatsHeader__individual-habits-container">
                         <div className="StatsHeader__habits-container">
-                            <IndividualHabit/>
-                            <IndividualHabit/>
-                            <IndividualHabit/>
-                            <IndividualHabit/>
-                            <IndividualHabit/>
-                            <IndividualHabit/>
+                            {this.displayHabits()}
                         </div> 
                     </div>
                     <div className="StatsHeader__total-stats-container">
