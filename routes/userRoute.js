b28ac76348fa2763c319d54251df7354c0eddc26
Laith Harb <harblaith77@gmail.com>
2020-04-03 16:52:20 -0400

    Finally figured out how to update subdocument in mongo

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const User = mongoose.model("users")
 const Habits = mongoose.model("habits")
 
-
+// GETTING ALL USER HABITS //
 router.get('/userhabits/:id', (req, res) => {
     User.findById({
         _id : req.params.id
@@ -13,17 +13,56 @@ router.get('/userhabits/:id', (req, res) => {
     })
 })
 
-router.patch('/addhabit/:id', async (req, res) => {
-    console.log(req.body)
-    console.log(req.params.id)
-    const response = await User.findByIdAndUpdate(req.params.id, {
-        habits : [...req.body.habits, req.body.habit]
-    })
-    console.log(response)
-    res.send(response)
-})
+router.patch('/updateScore/:userId/:habitId', async (req, res) => {
 
-// Flimsy code
+    const user = await User.findById(req.params.userId)
+    const habits = user.habits
+    
+    
+    const updatedHabits = habits.map((habit) => {
+        console.log('running')
+        console.log(habit._id)
+        console.log(req.params.habitId)
+        if(habit._id == req.params.habitId){
+            console.log('Running')
+            habit.currentScore = habit.currentScore + 5;
+            return habit
+        } else {
+            console.log('am i running?')
+            return habit
+        }
+    })
+    
+
+    const updatedSchema = await User.findByIdAndUpdate(
+        {_id : req.params.userId},
+        {habits : updatedHabits},
+        (err, result) => {
+            console.log(result)
+        }
+    )
+
+    const results = await updatedSchema.save()
+
+    res.send(results)
+
+    /*
+    const {currentScore} = req.body
+    let habit;
+    const user = await User.findById(req.params.userId, (err, user) => {
+        let habit = user.habits.find(habit => habit.habit === req.params.habitTitle)
+        habit.currentScore = 5
+    })
+    await user.save()
+    res.send(user)
+    */
+    
+})
+//5e8650488d3aa90a18549b89
+//5e8763cf8de899044ceadd4c
+
+// ADDING HABIT SUBDOCUMENT TO ARRAY //
+
 router.post('/addhabit/:id', async (req, res) => {
     const {habit, description, duration, linkedTo, color} = req.body;
 
@@ -35,11 +74,10 @@ router.post('/addhabit/:id', async (req, res) => {
         color
     })
 
-    console.log("I am running")
-
     const user = await User.findById(req.params.id)
     user.habits.push(newHabit);
     await user.save();
+
     res.send(user)
     
 })
