ddb331a5d7f8b9f6f150c14e997cbccebef4a6b6
Laith Harb <harblaith77@gmail.com>
2020-05-05 18:44:17 -0400

    Completed phase 1 of front-end functionality of tracker page

--- client/src/components/IndividualGoal/IndividualGoal.js
+++ client/src/components/IndividualGoal/IndividualGoal.js
@@ -2,27 +2,52 @@ import React, {useState} from 'react'
 import "./IndividualGoal.scss"
 import {motion, useMotionValue, useTransform, AnimatePresence} from "framer-motion"
 
-export default function IndividualGoal() {
+export default function IndividualGoal(props) {
 
     const x = useMotionValue(0)
     const opacity = useTransform(x, [-100, 0, 100], [0, 1, 0])
     const [isCardActive, setIsCardActive] = useState(true)
+    const [statusColor, setStatusColor] = useState("rgb(207, 123, 123)")
     console.log(x)
+
+    const changeStatus = () => {
+        const {status, id, timePeriod} = props.goal
+
+        if(status === "Not Complete") {
+            props.updateStatus(id, "In Progress", timePeriod)
+            setStatusColor("rgb(236, 143, 163)")
+
+        } else if (status === "In Progress") {
+            props.updateStatus(id, "Almost Complete", timePeriod)
+            setStatusColor("rgb(123, 207, 207)")
+
+        } else if (status === "Almost Complete") {
+            props.updateStatus(id, "Complete", timePeriod)
+            setStatusColor("rgb(143, 236, 182)")
+
+        } else {
+            props.updateStatus(id, "Not Complete", timePeriod)
+            setStatusColor("rgb(6, 151, 67)")
+        }
+    }
+
 
     return (
         <AnimatePresence>
-            <motion.div exit={{height:0, overflow: "hidden"}}>
+            <motion.div exit={{height:0, overflow: "hidden"}} onClick={changeStatus} >
                 {isCardActive && (
                     <motion.div 
                         drag = "x"
                         className="IndividualGoal"
                         style = {{
                             x,
-                            opacity
+                            opacity,
+                            backgroundColor: statusColor
                         }}
                         onDragEnd={(e, info) => {
                             if(info.point.x > 85){
                                 setIsCardActive(false)
+                                props.removeGoal(props.goal.id, props.goal.timePeriod)
                             }
                         }}
                         dragConstraints = {{
@@ -30,7 +55,7 @@ export default function IndividualGoal() {
                             right: 0
                         }}
                     >
-                        Complete Habit Section on Greatrr
+                        {props.goal.goal}
                     </motion.div>
                 )}
             </motion.div>
--- client/src/components/WeekGoals/WeekGoals.js
+++ client/src/components/WeekGoals/WeekGoals.js
@@ -2,8 +2,106 @@ import React, { Component } from 'react';
 import "./WeekGoals.scss"
 import IndividualGoal from "../IndividualGoal/IndividualGoal"
 import {motion, AnimatePresence} from "framer-motion"
+import uuid from "uuid/v4"
 
 class WeekGoals extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            goal: "",
+            timePeriod : "week",
+            weekGoals : [],
+            dayGoals : []
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value,
+            
+        })
+    }
+
+    handleSubmit = (e) => {
+        const {timePeriod, goal} = this.state
+        e.preventDefault()
+
+        if(!this.state.goal){
+            return 
+        }
+
+        if(timePeriod === "week"){
+            this.setState({
+                weekGoals : [
+                    ...this.state.weekGoals,
+                    {
+                        id: uuid(),
+                        status: "Not Complete",
+                        goal: goal,
+                        timePeriod : "weekGoals"
+                    }
+                ],
+                goal: ""
+            })
+        } else {
+            this.setState({
+                dayGoals : [
+                    ...this.state.dayGoals,
+                    {
+                        id: uuid(),
+                        status: "Not Complete",
+                        goal: goal,
+                        timePeriod: "dayGoals"
+                    }
+                ],
+                goal: ""
+            })
+        }
+    }
+
+    changeStatus = (id, newStatus, period) => {
+        console.log(this.state[period])
+        const index = this.state[period].findIndex(goal => {
+            return goal.id === id
+        })
+
+        const updatedState = this.state[period]
+
+        updatedState[index] = {
+            ...updatedState[index],
+            status: newStatus
+        }
+
+        this.setState({
+            [period] : updatedState
+        })
+    }
+
+    displayWeekGoals = () => {
+        return this.state.weekGoals.map(goal => {
+            return <IndividualGoal goal={goal} updateStatus = {this.changeStatus} removeGoal={this.removeGoal}/>
+        })
+    }
+
+    displayDayGoals = () => {
+        return this.state.dayGoals.map(goal => {
+            return <IndividualGoal goal={goal} updateStatus = {this.changeStatus} removeGoal={this.removeGoal}/>
+        })
+    }
+
+    removeGoal = (id, period) => {
+        console.log(period)
+        console.log(this.state[period])
+        const updatedState = this.state[period].filter(goal => {
+            return goal.id !== id
+        })
+
+        this.setState({
+            [period] : updatedState
+        })
+    }
+
     render() {
         return (
             <div className="WeekGoals">
@@ -11,17 +109,26 @@ class WeekGoals extends Component {
                     <h3 className="WeekGoals__heading">
                         Week & Daily Goals
                     </h3>
-                    <form action="" className="WeekGoals__form">
+                    <form action="" className="WeekGoals__form" onSubmit = {this.handleSubmit}>
                         <input 
                             type="text" 
                             className="WeekGoals__text-input"
                             placeholder="Add a new goal"
+                            onChange={this.handleChange}
+                            name="goal"
+                            value={this.state.goal}
                         />
-                        <select name="" id="" className="WeekGoals__select">
+                        <select 
+                            id="" 
+                            className="WeekGoals__select" 
+                            name="timePeriod" 
+                            value={this.state.timePeriod}
+                            onChange = {this.handleChange}
+                        >
                             <option value="week" className="WeekGoals__option">
                                 week
                             </option>
-                            <option value="week" className="WeekGoals__option">
+                            <option value="day" className="WeekGoals__option">
                                 day
                             </option>
                             
@@ -37,8 +144,7 @@ class WeekGoals extends Component {
                             </h4>
 
                                 <div className="WeekGoals__card" >
-                                    <IndividualGoal/>
-                                    <IndividualGoal/>
+                                   {this.displayWeekGoals()}
                                 </div>
 
                         </div>
@@ -47,7 +153,7 @@ class WeekGoals extends Component {
                                 Day
                             </h4>  
                             <div className="WeekGoals__card">
-                                
+                                 {this.displayDayGoals()}
                             </div> 
                         </div>
                     </div>
